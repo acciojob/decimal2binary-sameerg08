c2b88fde063ea4a045e7d4934a46d7c43a2f545c
Sameer Gupta <128709790+sameerg08@users.noreply.github.com>
2023-08-14 23:26:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let ans="";
+  while (num>0) {
+  	ans+=num%2;
+	num=Math.floor(num/2);
+  }
+	let fans="";
+	for(let i=ans.length-1;i>=0;i--){
+		fans+=ans[i];
+	}
+	console.log(fans);
 }
+
 
 window.decimalToBinary = decimalToBinary;
